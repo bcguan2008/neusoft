c25099c372714013378991817ef2112a59bf3d78
v_liyuanning <liyuanning@neusoft.com>
2016-09-02 20:02:18 +0800

    init commit

--- client/app/service/templateSvc.js
+++ client/app/service/templateSvc.js
@@ -9,9 +9,9 @@ class templateSvc  {
     this.location = $location;
     this.baseUrl = "/role/";
   }
-//保存模板
+//保存模板role/operate/act/save
   savetemplate(params){
-  	 return this.Api.post(this.baseUrl + 'save', params);
+  	 return this.Api.post(this.baseUrl + 'operate/act/save', params);
   }
   //显示所有模板 role/search
 	getPageAllTempList(params){
@@ -42,15 +42,19 @@ class templateSvc  {
 
 	postEdit(params){
 		// debugger;
-		console.log("postEdit");
-	    return this.Api.post(this.baseUrl+'/operate/act/save/rid/', params);
+	
+	    return this.Api.post(this.baseUrl+'operate/act/save/rid/'+params.rid, params);
 	}
-	changeStatus(params){
-	    return this.Api.post('/template/changeStatus', params);
+	changeStatus(params){ 
+		console.log(params.rid); 
+		console.log(params.status);  
+		//console.log(params.rid);
+	    return this.Api.get(this.baseUrl+'operate/act/update/rid/'+params.rid+/pause/+params.status, {});
 	}
 	//删除
 	remove_role(params){
-		  return this.Api.post(this.baseUrl+'/operate/act/remove/rid', params);
+			console.log(params.rid);
+		  return this.Api.post(this.baseUrl+'/operate/act/remove/rid/'+ params.rid, params);
 
 	}
 }
