d4ac1568afbe6395fb43aad0000cc4852933c50e
v_liyuanning <liyuanning@neusoft.com>
2016-09-04 12:00:15 +0800

    liyuanning

--- client/app/components/template/add/add.controller.js
+++ client/app/components/template/add/add.controller.js
@@ -5,22 +5,37 @@
  */
 
 export default class AddController {
-  constructor(Api,treeSvc,$scope) {
+  constructor(Api,treeSvc,$scope,templateSvc) {
   	"ngInject";
     this.name = 'add';
     this.treeSvc= treeSvc;
     this.form = {};
     this.api=Api;
     this.d={};
-    this.loadPromise = this.treeSvc.get()
+    this.templateSvc = templateSvc;
+        this.loadPromise = this.treeSvc.getSopTree('sop');
+        this.config = {
+            fieldOfChildren: 'children',
+            fieldOfName: 'name',
+            fieldOfId: 'nodeId'
+        };
+
+  }
+
+
+tabtree(){
+  console.log('app');
+    this.loadPromise = this.treeSvc.getAppTree('app');
     this.config = {
         fieldOfChildren: 'children',
         fieldOfName: 'name',
-        fieldOfId: 'id'
+        fieldOfId: 'nodeId'
     };
-  }
+
+}
+
   save(){
-    console.log(this.d);
+    console.log(this.d)
      this.api.post('/role/operate/act/save',this.d).then(res=>{alert('保存成功'),err=>{alert(err)}})
   }
 
--- client/app/components/template/edit/edit.controller.js
+++ client/app/components/template/edit/edit.controller.js
@@ -24,28 +24,24 @@ export default class EditController {
         this.form = {};
 
         //拉去列表数据的promise
-        this.loadPromise = this.treeSvc.get()
+        this.loadPromise = this.treeSvc.getselectSopTree(this.$state.params.id)
 
         //这个配置用于避免或减少数据转换
         this.config = {
-            // //指示子节点的字段名
-             fieldOfChildren: 'child',
-            // //指示节点名的字段
-             fieldOfName: 'categoryName',
-            // //指示节点id的字段
-            fieldOfId: 'categoryId'
+           //指示子节点的字段名
+            fieldOfChildren: 'child',
+            //指示节点名的字段
+            fieldOfName: 'name',
+            //指示节点id的字段
+            fieldOfId: 'nodeId'
 
         };
 
         
     }
-    save(){
-      console.log(this.d)
-     
-     this.templateSvc.postEdit(this.d);
+    save(){     
+      this.templateSvc.postEdit(this.d);
         //this.api.post('/role/operate/act/save/rid'+this.d.rid,this.d).then(res=>{alert('保存成功'),err=>{alert(err)}})
-
-
     }
 
 editInit(){
@@ -57,9 +53,10 @@ editInit(){
     }).then(result => {
           if(result){
             console.log(result)
+            console.log(result.name)
                _this.d={
                  name:result.name,
-                 describe:result.description,
+                 description:result.description,
                  rid : result.rid
                 // nodes:result.nodes
                }
