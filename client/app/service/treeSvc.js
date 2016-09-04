bef4438ea48eeb2d7a01f4fff8d322c7cadc63c1
v_liyuanning <liyuanning@neusoft.com>
2016-09-04 11:57:51 +0800

    liyuanning

--- client/app/service/treeSvc.js
+++ client/app/service/treeSvc.js
@@ -9,50 +9,22 @@ class treeSvc  {
     this.Api = Api;
   }
     //得到tree数据
-    get(params){
-      //模拟
-      var q = this.q.defer();
-      setTimeout(function(){
-          q.resolve([
-              {
-                  'name':'1级',
-                  'id':'407',
-                  'children':[
-                      {
-                        'name':'2级',
-                        'id':'409',
-                        'children':[
-                            {
-                                'name':'3级',
-                                'id':'410',
-                                'children':[]
-                            },
-                            {
-                                'name':'3级',
-                                'id':'410',
-                                'children':[]
-                            }
-                        ]       
-                      }
-                  ],
-              },{
-                  'name':'1级',
-                  'id':'407',
-                  'children':[
-                      {
-                        'name':'2级',
-                        'id':'409',
-                        'children':[
-                           
-                        ]       
-                      }
-                  ],
-              }
-          ])
-      },100);
-      return q.promise;
+  getSopTree(params){
+      return this.Api.get('/role/operate/act/nodes/type/sop',params)
   }
 
+  getAppTree(params){
+      return this.Api.get('/role/operate/act/nodes/type/app',params)
+  }
+  //已选 http://demo1015.sit.ffan.com/staffmgt/role/operate/act/nodes/type/{{sop/app}}/rid/23239
+  getselectSopTree(params){
+      return this.Api.get('/role/operate/act/nodes/type/sop/rid/'+params)
+  }
+   getselectAppTree(params){
+      return this.Api.get('/role/operate/act/nodes/type/app/rid/'+params)
+  }
 }
+
+//http://demo1015.sit.ffan.com/staffmgt/role/operate/act/nodes/type/{{sop/app}}
 
 export default treeSvc
