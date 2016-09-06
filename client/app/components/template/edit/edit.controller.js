30f7ced752e990b68bbac1cd257b22093d6c0a93
guanbingchang <guanbingchang@wanda.cn>
2016-09-06 19:18:29 +0800

    add

--- client/app/components/template/edit/edit.controller.js
+++ client/app/components/template/edit/edit.controller.js
@@ -16,56 +16,44 @@ export default class EditController {
       // this.api.get('/Template/detail',{templateNo:this.$state.params.id}).then(res=>{
       //     _this.d=res;
       // })
+      /**
       this.editInit();
       this.initTree();
+      */
+      this.init();
   }
-    initTree(){
-        //约定：所有抛到后端的都放到这个键下
-        this.form = {};
 
-        //拉去列表数据的promise
-        this.loadPromise = this.treeSvc.getselectSopTree(this.$state.params.id)
-
-        //这个配置用于避免或减少数据转换
-        this.config = {
-           //指示子节点的字段名
-            fieldOfChildren: 'children',
-            //指示节点名的字段
-            fieldOfName: 'name',
-            //指示节点id的字段
-            fieldOfId: 'nodeId'
-
-        };
-
+    init(){
+      let self = this;
+      self.loading = true;
         
+      self.loadPromise= self.templateSvc.getDetail({
+        id: this.$state.params.id
+      });
+
+      this.form = {};
+      this.loadPromise = this.treeSvc.getselectSopTree(this.$state.params.id)
+      this.loadPromise.then(results=>{
+        self.d.nodes = results;
+      })
+      this.config = {
+          fieldOfChildren: 'children',
+          fieldOfName: 'name',
+          fieldOfId: 'nodeId'
+      };
+
+      return self.loadPromise.then(result => {
+            if(result){
+              self.loading = false;
+              self.d.name = result.name;
+              self.d.description = result.description;
+              self.d.rid = result.rid;
+            }
+          });
     }
+    
     save(){     
        console.log(this.d);
        this.templateSvc.postEdit(this.d);
-      // alert('保存成功');
-       // this.api.post('/role/operate/act/save/rid'+this.d.rid,this.d).then(res=>{alert('保存成功'),err=>{alert(err)}})
     }
-
-editInit(){
-  
-  let self = this;
-  var _this = this;
-   self.loading = true;
-    self.loadPromise= self.templateSvc.getDetail({
-      id: this.$state.params.id
-    });
-    return self.loadPromise.then(result => {
-          if(result){
-            self.loading = false;
-               _this.d={
-                 name:result.name,
-                 description:result.description,
-                 rid : result.rid
-                // nodes:result.nodes
-               }
-
-          }
-        });
-    }
-
 }
