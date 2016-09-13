8d6077e2ca8ed049b0f414860ef59f060e67285a
v_liyuanning <liyuanning@neusoft.com>
2016-09-13 15:08:02 +0800

    fix 184 bug liyn

--- client/app/components/store/list/list.controller.js
+++ client/app/components/store/list/list.controller.js
@@ -5,19 +5,21 @@
  */
 
 export default class ListController {
-    constructor($q, storeSvc, $scope, $http, NgTableParams, $state,templateSvc,$window) {
+    constructor($q, storeSvc, $scope, $http,$httpParamSerializer,NgTableParams, $state,$window,staffnewSvc) {
         "ngInject";
         this.$state = $state
         this.storeSvc = storeSvc;
         this.window = $window;
+        this.scope = $scope;
         this.q = $q;
-        this.NgTableParams = NgTableParams;
+        //this.NgTableParams = NgTableParams;
+        this.httpParamSerializer = $httpParamSerializer;
         this.d={};
-        this.templateSvc=templateSvc;
+        this.staffnewSvc = staffnewSvc;
         this.init();
         this.filter = {
-            limit: 10,
-            offset:0
+            limit: 999999,
+          //  offset:0
             };
 
     }
@@ -33,32 +35,44 @@ export default class ListController {
         var self = this;
         var _this = this;
         var filter = this.filter;
-        this.tableParams = new this.NgTableParams({
-            page: 1,
-            offset:0 
-            //count: 10 //每页几条
-        }, {
-               // counts: [],
-                getData: function (params) {
-                    let formData = self.getSearchFormData(filter);//filter
-                    formData.page = params.url().page;
-                    formData.offset = params.url().page;
-                   self.loadPromise = self.storeSvc.getStoreInfoList(params);
-                   // self.loadPromise = self.templateSvc.getPageAllTempList(formData);
-                    return self.loadPromise
-                        .then(result => {
-                            self.loading = false;
-                            if (result.datas) {
-                                console.log(result)
-                                  _this.d={
-                                         totalCount:result.totalCount
-                                         }
-                                params.total(result.totalCount);
-                                return result.datas
-                            }
-                        });
-                }
+         // this.tableParams = new this.NgTableParams({
+         //   // page: 1,
+         //    //offset:0 
+         //    //count: 10 //每页几条
+         // }, {
+         //        counts: [],
+         //        getData: function (params) {
+         //            let formData = self.getSearchFormData(filter);//filter
+         //            //formData.offset = (params.url().page - 1) * params.url().page;
+         //            //formData.limit = params.url().count;                   
+         //           self.loadPromise = self.storeSvc.getStoreAllList(formData);
+  
+         //            return self.loadPromise
+         //                .then(result => {
+         //                    self.loading = false;
+         //                    if (result.datas) {
+         //                        console.log(result)
+         //                          _this.d={
+         //                                 totalCount:result.totalCount
+         //                                 }
+         //                        params.total(result.totalCount);
+         //                        return result.datas
+         //                    }
+         //                });
+         //       }
+         //    })
+
+        self.loadPromise = self.storeSvc.getStoreAllList({
+             limit: 999999
             })
+        self.loadPromise.then((result) => {
+          _this.d={
+            totalCount:result.totalCount
+          }
+            this.scope.$data = result.datas;
+        });   
+
+         
     }
     search() {
         this.storeSvc.getStoreInfoList();
@@ -70,7 +84,7 @@ export default class ListController {
         this.$state.go('storeedit', { id: id });
     }
     getstaffpageadd() {
-        this.staffnewSvc.getstaffpage()
+        this.staffnewSvc.getstaffpage() 
     }
     goStaff(storeid) {
         this.$state.go('stafflist', { storeid: storeid });
@@ -81,10 +95,7 @@ export default class ListController {
   exportExcel(){
    // debugger;
     let formData = this.getSearchFormData();
-        formData.page = this.tableParams.page();
-        //return this.Api.get(this.baseUrl+'stafflist?format=excel');  
-       
-    this.window.open('/Staffmgt/Organization/storelist?format=excel', '_blank');
+    this.window.open('/Staffmgt/Organization/storelist?format=excel&'+ this.httpParamSerializer(formData), '_blank');
   
         //this.storeSvc.exportExcellist()
   }
