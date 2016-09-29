d79ed4b9e49ac8f55e0e7769e98b94d505940ce5
liyuanning <liyuanning@163.com>
2016-09-29 11:07:52 +0800

    liyn 员工激励

--- client/app/components/staffExcitation/list/list.controller.js
+++ client/app/components/staffExcitation/list/list.controller.js
@@ -5,106 +5,236 @@
  */
 
 export default class ListController {
-  constructor($scope,$http,staffExcitationSvc,NgTableParams,$httpParamSerializer) {
+  constructor($scope,$http,$q,staffExcitationSvc,NgTableParams,
+    $httpParamSerializer,storeSvc,$window) {
   	 "ngInject";
-     this.name = 'list';
-     this.staffExcitationSvc = staffExcitationSvc;
-     this.NgTableParams = NgTableParams;
-    this.httpParamSerializer = $httpParamSerializer;
+      this.name = 'list';
+      this.staffExcitationSvc = staffExcitationSvc;
+      this.NgTableParams = NgTableParams;
+      this.httpParamSerializer = $httpParamSerializer;
+      this.storeSvc= storeSvc;
+      this.q = $q;
+      this.window = $window;
+      this.filter = {
+       offset: 0 ,
+       employeeName:'',
+       phone:'',
+       store:'',
+       laterThan:'',
+       earlierThan:''
+      };
+      this.d={}
+      this.filterBymonth = {
+       offset: 0
+      };
+      this.month ={}
+      
+      this.initBymonth(); //本月汇总
+      this.initSearch();
 
-     this.filter = {
-      limit: 10,
-      name: '',
-      phone: '',
-      action_1: '',
-      button_name: '',
-      status: '',
-      update_start_time: '',
-      update_end_time: ''
-    };
-
-    this.list = [];   //当前页数据
-    this.allIds = []; //当前页所有id
-    this.init();
-
-     $scope.vm.staffExcitation = [{ id: 1, name: 'name 1' }, { id: 2, name: 'name 2' }, { id: 3, name: 'name 3' }, { id: 4, name: 'name 4' }];
-     $scope.vm.action = [{ id: 1, name: '首单支付' }, { id: 2, name: '扫码激励' }, { id: 3, name: '拉新激励' }, { id: 4, name: '拉单激励' }];
-     $scope.vm.items = [
-        {"id":"1","name":"name 1","description":"description 1","field3":"field3 1","field4":"field4 1","field5 ":"field5 1"}, 
-        {"id":"2","name":"name 2","description":"description 1","field3":"field3 2","field4":"field4 2","field5 ":"field5 2"}, 
-        {"id":"3","name":"name 3","description":"description 1","field3":"field3 3","field4":"field4 3","field5 ":"field5 3"}, 
-        {"id":"4","name":"name 4","description":"description 1","field3":"field3 4","field4":"field4 4","field5 ":"field5 4"}, 
-        {"id":"5","name":"name 5","description":"description 1","field3":"field3 5","field4":"field4 5","field5 ":"field5 5"}, 
-        {"id":"6","name":"name 6","description":"description 1","field3":"field3 6","field4":"field4 6","field5 ":"field5 6"}, 
-        {"id":"7","name":"name 7","description":"description 1","field3":"field3 7","field4":"field4 7","field5 ":"field5 7"}, 
-        {"id":"8","name":"name 8","description":"description 1","field3":"field3 8","field4":"field4 8","field5 ":"field5 8"}, 
-        {"id":"9","name":"name 9","description":"description 1","field3":"field3 9","field4":"field4 9","field5 ":"field5 9"}, 
-        {"id":"10","name":"name 10","description":"description 1","field3":"field3 10","field4":"field4 10","field5 ":"field5 10"}, 
-        {"id":"11","name":"name 11","description":"description 1","field3":"field3 11","field4":"field4 11","field5 ":"field5 11"}, 
-    ];
+     //this.action = [{ id: 0, name: '' },{ id: 1, name: '首单支付' }, { id: 2, name: '扫码激励' }, { id: 3, name: '拉新激励' }, { id: 4, name: '拉单激励' }];
+    //  $scope.vm.items = [
+    //     {"id":"一月","name":"1","description":"description 1","field3":"field3 1","field4":"field4 1","field5 ":"field5 1"}, 
+    //     {"id":"二月","name":"性别女","description":"description 1","field3":"field3 2","field4":"field4 2","field5 ":"field5 2"}, 
+    //     {"id":"二月","name":"性别女","description":"description 1","field3":"field3 3","field4":"field4 3","field5 ":"field5 3"}, 
+    //     {"id":"二月","name":"性别女","description":"description 1","field3":"field3 4","field4":"field4 4","field5 ":"field5 4"}, 
+    //     {"id":"18200000006","name":"4","description":"description 1","field3":"field3 5","field4":"field4 5","field5 ":"field5 5"}, 
+    //     {"id":"性别女","name":"5","description":"description 1","field3":"field3 6","field4":"field4 6","field5 ":"field5 6"}, 
+    //     {"id":"性别女","name":"6","description":"description 1","field3":"field3 7","field4":"field4 7","field5 ":"field5 7"}, 
+    //     {"id":"性别女","name":"7","description":"description 1","field3":"field3 8","field4":"field4 8","field5 ":"field5 8"}, 
+    //     {"id":"9","name":"7","description":"description 1","field3":"field3 9","field4":"field4 9","field5 ":"field5 9"}, 
+    //     {"id":"10","name":"8","description":"description 1","field3":"field3 10","field4":"field4 10","field5 ":"field5 10"}, 
+    //     {"id":"11","name":"11","description":"description 1","field3":"field3 11","field4":"field4 11","field5 ":"field5 11"}, 
+    // ];
+   // this.month =  [{ id: 1, name: '一月' }, { id: 2, name: '二月' }, { id: 3, name: '三月' }, { id: 4, name: '四月' }, { id: 5, name: '五月' }, { id: 6, name: '六月' }, { id: 7, name: '七月' }, { id: 8, name: '八月' }, { id: 9, name: '九月' }, { id: 10, name: '十月' }, { id: 11, name: '十一月' }, { id: 18200000006, name: '十二月' }];
   }
  /**
    * 获取格式化后的数据
    */
+  getSearchFormDataBymonth(){
+    let filter = this.filterBymonth
+     var now = new Date()
+     var curYear = now.getFullYear() 
+     console.log(now.getFullYear())
+     var curMonth = now.getMonth()+1 //从0开始，因此需要+1
+      //获取当前月份传给后台
+    let selectmonth = this.month; 
+    console.log(selectmonth.value)
+    if(selectmonth.value != undefined)
+    {
+       filter.month = selectmonth.value;  
+    }else{
+      filter.month = curYear +"-" + curMonth; 
+    }
+    return filter
+
+  }
+
   getSearchFormData(){
-    let filter = this.filter,
-        selectObj = this.selectObj;
+    let filter = {
+      merchantId:this.d.merchantId,
+      employeeName:this.filter.employeeName,
+      phone:this.filter.phone,
+      storeName:this.filter.store
 
-    filter.track_page_type_id = selectObj.pageType.id;
-    filter.business_code = selectObj.business.business_code;
+    }
+    // filter.bizAccountName=this.filter.bizAccountName;
+    // filter.phone=this.filter.phone;
+    // filter.storeName=this.filter.store; // 门店名称
 
+ 
+    // let selectAction = this.selectAction; 
+    // if(selectAction.value != undefined)
+    // {
+    //    filter.status = selectAction.value.name;  
+    // }
+
+   
+    filter.laterThan = this.filter.laterThan; //开始时间
+    filter.earlierThan = this.filter.earlierThan; //结束时间
     return filter;
   }
 
   /**
    * [init 初始化]
    */
-  init(){
-  	var vm = this;
-    let self = this;
-    this.tableParams = new this.NgTableParams({
-      count: 10 //每页几条
-    }, {
-      counts:[],
-      getData: function(params) {
-      //  console.log(params.url().page);
-        console.log(vm.filter.action_1.name)
-        return self.staffExcitationSvc.getExcitationList(vm.filter)
-        .then(result => {
-          if(result && result.list){
-          	 console.log(result.total)
-            params.total(result.total);
-            return result.list;
+  initBymonth(){
+
+     let self = this;
+      this.tableParams = new this.NgTableParams({
+        page: 1,
+        count: 10
+      }, {
+          getData: function ($defer, params) {
+            let paramsUrl = params.url();
+            self.loading = true;
+            let formData = self.getSearchFormDataBymonth();
+            formData.offset = paramsUrl.page;
+            formData.limit = paramsUrl.count;
+            console.log(formData)
+            //员工奖励汇总
+            self.loadPromise = self.staffExcitationSvc.getBestirByMonth(formData);
+            return self.loadPromise 
+              .then(result => {
+                self.loading = false;
+                if (result) {
+                 //debugger;
+                  self.totalCount = result.totalCount
+                  params.total(result.totalCount);
+
+                  return result.datas; 
+                }
+              });
           }
         });
-      }
-    });
   }
 
-  search_Excitation(){
+initSearch(){
+     
+     let self = this;
+      this.tableParamsSearch = new this.NgTableParams({
+        page: 1,
+        count: 10
+      }, { 
+          getData: function ($defer, params) {
+             let totalAmount=0;
+            let paramsUrl = params.url();
+            self.loading = true;
+            let formData = self.getSearchFormData();//filter
+
+            formData.offset = paramsUrl.page;
+            formData.limit = paramsUrl.count;
+            //员工奖励汇总 
+           //  console.log(formData) 
+           //  console.log(self.staffExcitationSvc.getBestirList(formData))
+            self.loadPromiseSearch = self.staffExcitationSvc.getBestirList(formData);
+            return self.loadPromiseSearch
+              .then(result => {
+                self.loading = false;
+                if (result) {
+              
+                    result.items.forEach((item)=>{
+                              if (totalAmount==0)
+                              {
+                                 totalAmount = item.amount
+                              }else{
+                                 totalAmount = totalAmount+item.amount
+
+                              }
+                            })
+                  self.d = {
+                    totalAmount:totalAmount,
+                    totalCountSearch: result.totalCount,
+                    merchantId:result.items[0].merchantId
+                  }
+
+                  console.log(result.items[0].merchantId)
+                  params.total(result.totalCount);
+                  return result.items;
+                }
+              });
+          }
+        });
+  }
+  searchBymonth(){
+        //return staffExcitationSvc.getExcitationList(formData)
+
+         this.tableParams.parameters({page : 1}).reload();
+  }
+  searchExcitation(){
+    
+    let laterThan=new Date(this.filter.laterThan)
+    let earlierThan=new Date(this.filter.earlierThan)
+   
+    if(Date.parse(earlierThan) < Date.parse(laterThan))
+    {
+      alert("结束时间要大于开始时间！")
+      return false;
+    }
   			//return staffExcitationSvc.getExcitationList(formData)
-  			 this.tableParams.parameters({page : 1}).reload();
+  	this.tableParamsSearch.parameters({page : 1}).reload();
+  }
+  /**
+   * [reset 重置]
+   */
+  reset(){
+    this.filter = {
+      name: '',
+      phone: '',
+      store:'',
+      update_start_time: '',
+      update_end_time: ''
+    };
+    // this.selectAction={}
+  }
+  /**
+   * [downloadExcel]
+   */
+  exportExcelMonth(){
+   // debugger;
+    let formData = this.getSearchFormData();
+        formData.page = this.tableParams.page(); 
+        formData.limit = this.tableParams.data.length;
+    this.window.open('/Xapi/encourage/month_total?format=excel&'+ this.httpParamSerializer(formData), '_blank');
+  }
+
+    exportExcelSearch(){
+   // debugger;
+    let formData = this.getSearchFormData();
+        formData.page = this.tableParams.page(); 
+        formData.limit = this.tableParams.data.length;
+    this.window.open('/Xapi/encourage/list?format=excel&'+ this.httpParamSerializer(formData), '_blank');
+  }
+
+    //获取门店list
+  getStorelist(storename) {
+    let deferred = this.q.defer();
+    let storeList = this.storeSvc.getstorebyname(storename).then((result) => {
+      return result.datas;
+    });
+    deferred.resolve(storeList);
+    return deferred.promise;
   }
 
 
-		  /**
-		   * [reset 重置]
-		   */
-		  reset(){
-		    this.filter = {
-		      limit: 10,
-		      name: '',
-		      phone: '',
-		      action_1: '',
-		      status: '',
-		      update_start_time: '',
-		      update_end_time: ''
-		    };
-		}
-  // 导出Excel
-		exportExcel(){
-        this.window.open('/staffExcitation/staffExcitation/export?' + this.httpParamSerializer(formData), '_blank');
-		
-
-		}
 }
--- client/app/components/staffExcitation/personlist/list.controller.js
+++ client/app/components/staffExcitation/personlist/list.controller.js
@@ -5,72 +5,92 @@
  */
 
 export default class ListController {
-  constructor($scope,$http,staffExcitationSvc,NgTableParams) {
+  constructor($scope,$http,staffExcitationSvc,NgTableParams,$window,$httpParamSerializer) {
   	 "ngInject";
      this.name = 'list';
      this.staffExcitationSvc = staffExcitationSvc;
      this.NgTableParams = NgTableParams;
-
+     this.window=$window;
+     this.httpParamSerializer=$httpParamSerializer;
      this.filter = {
-      limit: 10,
-      name: '',
-      phone: '',
-      action_1: '',
-      button_name: '',
-      status: '',
-      update_start_time: '',
-      update_end_time: ''
+       offset: 0
     };
-  this.init();
+    this.selectMonth = {};
+    this.selectTask={}
+    this.init(); 
 
-     $scope.vm.staffExcitation = [{ id: 1, name: 'name 1' }, { id: 2, name: 'name 2' }, { id: 3, name: 'name 3' }, { id: 4, name: 'name 4' }];
-     $scope.vm.action = [{ id: 1, name: '一般好' }, { id: 2, name: '良' }, { id: 3, name: '优秀' }];
-     $scope.vm.items = [
-        {"id":"1","name":"name 1","description":"2016/8/9 11：12","field3":"field3 1","field4":"field4 1","field5 ":"field5 1"}, 
-        {"id":"2","name":"name 1","description":"2016/8/19 11：11","field3":"field3 2","field4":"field4 2","field5 ":"field5 2"}, 
-        {"id":"3","name":"name 3","description":"2016/1/19 13：12","field3":"field3 3","field4":"field4 3","field5 ":"field5 3"}, 
-        {"id":"4","name":"name 4","description":"2016/6/11 15：12","field3":"field3 4","field4":"field4 4","field5 ":"field5 4"}, 
-        {"id":"5","name":"name 2","description":"description 1","field3":"field3 5","field4":"field4 5","field5 ":"field5 5"}, 
-        {"id":"6","name":"name 2","description":"description 1","field3":"field3 6","field4":"field4 6","field5 ":"field5 6"}, 
-        {"id":"7","name":"name 2","description":"description 1","field3":"field3 7","field4":"field4 7","field5 ":"field5 7"}, 
-        {"id":"8","name":"name 8","description":"description 1","field3":"field3 8","field4":"field4 8","field5 ":"field5 8"}, 
-        {"id":"9","name":"name 9","description":"description 1","field3":"field3 9","field4":"field4 9","field5 ":"field5 9"}, 
-        {"id":"10","name":"name 10","description":"description 1","field3":"field3 10","field4":"field4 10","field5 ":"field5 10"}, 
-        {"id":"11","name":"name 11","description":"description 1","field3":"field3 11","field4":"field4 11","field5 ":"field5 11"}, 
-    ];
+     // $scope.vm.staffExcitation = [{ id: 1, name: 'name 1' }, { id: 2, name: 'name 2' }, { id: 3, name: 'name 3' }, { id: 4, name: 'name 4' }];
+     // $scope.vm.action = [{ id: 1, name: '一般好' }, { id: 2, name: '良' }, { id: 3, name: '优秀' }];
+  }
+  getSearchFormData()
+  {
+    
+    let filter  = {
+      month:this.filter.month,
+      task:this.filter.task,
+    }
+//获取 选择月份
+     let selectMonth = this.selectMonth; 
+    if (selectMonth.value != undefined) {
+      filter.month = selectMonth.value;
+    }else{
+      filter.month = 0
+    }
+//获取激励任务
+    let selectTask = this.selectTask; 
+    
+    if (selectTask.value != undefined) {
+      filter.task = selectTask.value;
+    }else{
+      filter.task = 0
+    }
+
+   //  console.log( filter)
+    return filter;
+
   }
 
-
-  /**
+   /**
    * [init 初始化]
    */
   init(){
-  	var vm = this;
-    let self = this;
-    this.tableParams = new this.NgTableParams({
-      count: 10 //每页几条
-    }, {
-      counts:[],
-      getData: function(params) {
-      //  console.log(params.url().page);
-        console.log(vm.filter.action_1.name)
-        return self.staffExcitationSvc.getExcitationList(vm.filter)
-        .then(result => {
-          if(result && result.list){
-          	 console.log(result.total)
-            params.total(result.total);
-            return result.list;
+
+     let self = this;
+      this.tableParams = new this.NgTableParams({
+        page: 1,
+        count: 10
+      }, {
+          getData: function ($defer, params) {
+        
+            let paramsUrl = params.url();
+            self.loading = true;
+            let formData = self.getSearchFormData();
+           
+           formData.offset = paramsUrl.page;
+            formData.limit = paramsUrl.count;
+             console.log(formData)
+            //员工个人奖励汇总
+            self.loadPromise = self.staffExcitationSvc.getBestirCollect(formData);
+            return self.loadPromise
+              .then(result => {
+                self.loading = false;
+                if (result) {
+                  self.d = {
+                    totalCount: result.totalCount
+                  }
+                  params.total(result.totalCount);
+                  //需要汇总的数据
+                  return result.datas;
+                }
+              });
           }
         });
-      }
-    });
   }
 
-  search_Excitation(){
-  			//return staffExcitationSvc.getExcitationList(formData)
-  			 this.tableParams.parameters({page : 1}).reload();
-  }
 
+  search() {
+    this.tableParams.parameters({ page: 1 }).reload();
+  }
 
 		  /**
 		   * [reset 重置]
@@ -87,9 +107,13 @@ export default class ListController {
 		    };
 		}
   // 导出Excel
-		exportExcel(){
+  exportExcel(){
+   // debugger;
+    let formData = this.getSearchFormData();
+        formData.page = this.tableParams.page(); 
+        formData.limit = this.tableParams.data.length;
+        console.log(formData)
+    this.window.open('/Staffmgt/Employee/stafflist?format=excel&'+ this.httpParamSerializer(formData), '_blank');
 
-		
-
-		}
+  }
 }
